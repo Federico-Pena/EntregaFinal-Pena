03842e68ad72947c2503428249bebca50845f7fd
federico-pena <federpena22@gmail.com>
2023-07-30 22:30:42 -0300

    sw

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,22 @@
 module.exports = {
 	globDirectory: './',
 	globPatterns: [
-		'**/*.{png,ico,txt,webp,css,html,js,json}'
+		'**/*.{html,css,js,png,webp,ico}', // Aquí los tipos de archivos que desees precachear
 	],
 	swDest: 'sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
-};
+	clientsClaim: true,
+	skipWaiting: true,
+	runtimeCaching: [
+		{
+			urlPattern: /^https:\/\/entrega-final-pena\.vercel.app/, // Dominio
+			handler: 'NetworkFirst',
+			options: {
+				cacheName: 'la-tienda-cache-v1', // Nombre para la caché
+				networkTimeoutSeconds: 10,
+				cacheableResponse: {
+					statuses: [0, 200],
+				},
+			},
+		},
+	],
+}
