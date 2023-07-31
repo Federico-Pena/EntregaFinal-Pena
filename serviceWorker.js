25ba8d60063b1175ac3f098106ab4b8e4ea57e68
federico-pena <federpena22@gmail.com>
2023-07-31 00:00:47 -0300

    sw

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,43 +1,25 @@
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
-		navigator.serviceWorker
-			.register('/sw.js')
-			.then((registration) => {
-				console.log('Service Worker registrado con éxito:', registration.scope)
-			})
-			.catch((error) => {
-				console.error('Error al registrar el Service Worker:', error)
-			})
+		navigator.serviceWorker.register('/sw.js')
 	})
 
 	// Evento para mostrar el prompt de instalación
 	let deferredPrompt
 	window.addEventListener('beforeinstallprompt', (e) => {
-		// Previene que el prompt se muestre automáticamente
 		e.preventDefault()
-		// Guarda el evento para mostrar el prompt más tarde
 		deferredPrompt = e
-		// Puedes mostrar un botón o alguna otra interacción para que el usuario lo inicie
-		// Por ejemplo, mostrar un botón "Instalar" y agregar un evento clic para que el usuario lo vea
 		showInstallButton(deferredPrompt)
 	})
 }
 function showInstallButton(deferredPrompt) {
-	// Muestra el botón de instalación y agrega un evento clic para mostrar el prompt
 	const installButton = document.getElementById('install-button')
 	installButton.style.display = 'block'
 	installButton.addEventListener('click', () => {
-		// Muestra el prompt cuando el usuario hace clic en el botón
 		deferredPrompt.prompt()
-		// Espera a que el usuario responda al prompt
 		deferredPrompt.userChoice.then((choiceResult) => {
 			if (choiceResult.outcome === 'accepted') {
-				console.log('El usuario aceptó la instalación')
 				installButton.style.display = 'none'
-			} else {
-				console.log('El usuario rechazó la instalación')
 			}
-			// Limpiar la referencia del evento para que no se muestre nuevamente automáticamente
 			deferredPrompt = null
 		})
 	})
